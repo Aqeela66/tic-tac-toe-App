6332d6b484fb5164f2f486f4e8f6cf587384008f
Aqeela <83452367+Aqeela66@users.noreply.github.com>
2021-09-11 02:39:48 +0500

    Tic Tac Toe Game App build using react.js

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,37 +1,41 @@
 import { useState } from 'react';
-import { calculateWinner } from './Helpers';
+
+
 import Board from './Board';
+
+import { calculateWinner } from './Helpers';
 
 const styles={
     width: '200px',
-    margin:'18px auto',
+    margin:'20px auto',
 }
 const Game=()=> {
    const [history, setHistory] = useState([Array(9).fill(null)]);
    const [stepNumber,setStepNumber]= useState(0);
    const [xIsNext, setXisNext]=  useState(true);
    const winner = calculateWinner(history[stepNumber]);
-    const handleClick = i =>{
-        const timeInHistory =history.slice(0,stepNumber + 1)
+    const handleClick= i =>{
+        const timeInHistory =history.slice(0,stepNumber+1)
         const current = timeInHistory[stepNumber];
         const squares =[...current];
-       
+       // const boardCopy =[...board]
+        //if user click an occupied square or if game is won,return
         if(winner || squares[i])
         return;
-        
+        //put an x or an 0 in the clicked square 
         squares[i]= xIsNext ? 'x' : '0';
         setHistory([...timeInHistory,squares]);
         setStepNumber(timeInHistory.length)
         setXisNext(!xIsNext);
 
     }
-    const jumpTo = step => {
+    const jumpTo=step=> {
         setStepNumber(step);
-        setXisNext(step % 2 === 0)
-    };
+        setXisNext(step % 2 ===0)
+    }
    const renderMoves=()=> (
        history.map((__step,move)=>{
-           const destination = move ? `Go to move${move}` : 'Start Game';
+           const destination =move ? `Go to move${move}` : 'Go to start';
            return(
                <li key={move}>
                    <button onClick={()=> jumpTo(move)}>{destination}</button>
@@ -43,15 +47,20 @@ const Game=()=> {
         
     
     return (
+        
         <>
+        <div style={styles}>
+        <h1>{ 'TIC TAC TOE'}</h1>
+        </div>
         <Board  squares={history[stepNumber]} onClick={handleClick} />
        <div style={styles}>
-           <p>{winner ? 'Winner: ' + winner :'Next Player: ' +(xIsNext ? 'x' : '0')}</p>
+       
+           <p>{winner ? 'winner:' + winner :'Next Player:' +(xIsNext ?'x': '0')}</p>
            {renderMoves()}
        </div>
         </>
     )
     }
 
-
 export default Game;
+
