48339e421e2148dc19935262611c57fee69feb79
Aqeela <83452367+Aqeela66@users.noreply.github.com>
2021-09-11 02:24:29 +0500

    Tic Tac Toe Game App build using react.js

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,36 +1,34 @@
 import { useState } from 'react';
-
-import Board from './Board';
 import { calculateWinner } from './Helpers';
+import Board from './Board';
 
 const styles={
     width: '200px',
-    margin:'20px auto',
+    margin:'18px auto',
 }
 const Game=()=> {
    const [history, setHistory] = useState([Array(9).fill(null)]);
    const [stepNumber,setStepNumber]= useState(0);
    const [xIsNext, setXisNext]=  useState(true);
    const winner = calculateWinner(history[stepNumber]);
-    const handleClick= i =>{
-        const timeInHistory =history.slice(0,stepNumber+1)
+    const handleClick = i =>{
+        const timeInHistory =history.slice(0,stepNumber + 1)
         const current = timeInHistory[stepNumber];
         const squares =[...current];
        
-        //if user click an occupied square or if game is won,return
         if(winner || squares[i])
         return;
-        //put an x or an 0 in the clicked square 
+        
         squares[i]= xIsNext ? 'x' : '0';
         setHistory([...timeInHistory,squares]);
         setStepNumber(timeInHistory.length)
         setXisNext(!xIsNext);
 
     }
-    const jumpTo=step=> {
+    const jumpTo = step => {
         setStepNumber(step);
         setXisNext(step % 2 === 0)
-    }
+    };
    const renderMoves=()=> (
        history.map((__step,move)=>{
            const destination = move ? `Go to move${move}` : 'Start Game';
@@ -48,11 +46,12 @@ const Game=()=> {
         <>
         <Board  squares={history[stepNumber]} onClick={handleClick} />
        <div style={styles}>
-       <p>{winner ? 'Winner: ' + winner :'Next Player: ' +(xIsNext ? 'x' : '0')}</p>
+           <p>{winner ? 'Winner: ' + winner :'Next Player: ' +(xIsNext ? 'x' : '0')}</p>
            {renderMoves()}
        </div>
         </>
     )
     }
 
-export default Game
+
+export default Game;
